3a0077a10ec44bbea6d2b11ce29a1244b51693fe
Robert <robert.aleks.zuk@gmail.com>
2022-06-07 12:22:42 +0200

    midify footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="text-center py-3">
         created by <span className="footer-name">Robert Żuk</span> -{" "}
-        <Link className="footer-link" to="https://devchallenges.io">
+        <a
+          className="footer-link"
+          href="https://devchallenges.io"
+          target="_blank"
+          rel="noreferrer"
+        >
           devChallenges.io
-        </Link>
+        </a>
       </div>
     </footer>
   );
